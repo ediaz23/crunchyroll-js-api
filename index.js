210ef7f0b4183caf4280c10654e67c09905ed2e9
ediaz23 <esteban.diazt23@gmail.com>
2024-03-06 10:40:52 -0300

    [DEL] remove unnecessary files

--- index.js
+++ index.js
@@ -3,15 +3,10 @@ import CrunchyrollError from './src/error.js'
 import logger from './src/logger.js'
 import localStore from './src/localStore.js'
 import types from './src/types.js'
-import production from './src/config/app-config-default-production.json'
-import configJson from './src/config/app-config-json-schema.json'
 import configApp from './src/config/config.json'
-import i18n from './src/config/index.i18n.json'
 import account from './src/services/account.js'
 import assets from './src/services/assets.js'
 import auth from './src/services/auth.js'
-import oldCms from './src/services/old.cms.js'
-import oldContent from './src/services/old.content.js'
 import content from './src/services/content.js'
 import discover from './src/services/discover.js'
 import music from './src/services/music.js'
@@ -34,16 +29,9 @@ export const api = {
     music,
     review,
     subscription,
-    old: {
-        cms: oldCms,
-        content: oldContent,
-    }
 }
 export const config = {
-    production,
-    json: configJson,
     configApp,
-    i18n,
 }
 export { logger }
 export { localStore }
